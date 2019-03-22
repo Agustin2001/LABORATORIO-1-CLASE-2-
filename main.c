7b9aca8254fe5a0bea329b779c03f1e9b43a09a8
sofia <sofia@sofia.com>
2019-03-21 21:17:24 -0300

    Clase1Labo

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+    int numero;
+    int max;
+    int contador=0;
+
+    do
+    {
+        printf("Ingresalo:");
+
+        scanf("%d",&numero);
+
+        if(contador==0)
+        {
+            max=numero;
+        }
+        else
+        {
+            if(numero > max)
+            {
+                max = numero;
+            }
+
+        }
+
+        contador++;
+    }while(numero != -1);
+
+            printf("El numero max es: %d ", max);
+
+    return 0;
+}
+
+
+
